dbb0fca894aef098a19f70df34e72a5dbb5468aa
Ir200on <snezokdasha@gmail.com>
2025-05-01 20:02:20 +0300

    first commit

---
+++ ai-startup/src/sections/Header.tsx
@@ -0,0 +1,19 @@
+import LogoIcon from "@/assets/logo.svg";
+
+export const Header = () => {
+  return (
+    <header className="py-4">
+      <div className="container">
+        <div className="flex justify-between">
+          <div>
+            <span>logo</span>
+          </div>
+          <div>
+            <button>Join waitlist</button>
+            <span>menu</span>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
