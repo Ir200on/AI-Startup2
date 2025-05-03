b9b200fc8d753f6a297b674cb9debf12cd180a1a
Ir200on <snezokdasha@gmail.com>
2025-05-03 15:07:13 +0300

    HeroSectionDone

--- ai-startup/src/sections/Hero.tsx
+++ ai-startup/src/sections/Hero.tsx
@@ -4,7 +4,7 @@ import starsBg from "@/assets/stars.png";
 export const Hero = () => {
   return (
     <section
-      className="h-[495px] flex items-center overflow-hidden relative [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]"
+      className="h-[495px] md:h-[800px] flex items-center overflow-hidden relative [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]"
       style={{
         backgroundImage: `url(${starsBg.src})`,
       }}
