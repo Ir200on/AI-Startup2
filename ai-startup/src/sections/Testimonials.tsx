30ef43b433dc35189a87e8078403d4f6f1c3112e
Ir200on <snezokdasha@gmail.com>
2025-05-05 02:56:48 +0300

    FixApos

--- ai-startup/src/sections/Testimonials.tsx
+++ ai-startup/src/sections/Testimonials.tsx
@@ -40,7 +40,7 @@ export const Testimonials = () => {
           Beyond Expectations.
         </h2>
         <p className="text-white/70 text-lg md:text-xl text-center mt-5 tracking-tight max-w-sm mx-auto">
-          Our revolutionary AI SEO tools have transformed our client's
+          Our revolutionary AI SEO tools have transformed our client&apos;s
           strategies
         </p>
 
