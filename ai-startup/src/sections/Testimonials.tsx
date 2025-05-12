15b4d12457028ac64d18144aab21705371be8c36
Ir200on <snezokdasha@gmail.com>
2025-05-13 02:23:08 +0300

    hate that. Fixes and TestimonialsAnimationDone

--- ai-startup/src/sections/Testimonials.tsx
+++ ai-startup/src/sections/Testimonials.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
-
+"use client";
 import avatar1 from "@/assets/avatar-1.png";
 import avatar2 from "@/assets/avatar-2.png";
 import avatar3 from "@/assets/avatar-3.png";
 import avatar4 from "@/assets/avatar-4.png";
+import Image from "next/image";
+import { motion } from "framer-motion";
 
 const testimonials = [
   {
@@ -44,9 +45,22 @@ export const Testimonials = () => {
           strategies
         </p>
 
-        <div className="overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-          <div className="flex gap-5">
-            {testimonials.map((testimonial) => (
+        <div className="flex overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+          <motion.div
+            initial={{
+              translateX: "-50%",
+            }}
+            animate={{
+              translateX: "0",
+            }}
+            transition={{
+              repeat: Infinity,
+              ease: "linear",
+              duration: 30,
+            }}
+            className="flex gap-5 pr-5 flex-none"
+          >
+            {[...testimonials, ...testimonials].map((testimonial) => (
               <div
                 key={testimonial.name}
                 className="border border-white/15 p-6 md:p-10 rounded-xl bg-[linear-gradient(to_bottom_left,rgba(140,69,255,.3),black)] max-w-xs md:max-w-md flex-none"
@@ -72,7 +86,7 @@ export const Testimonials = () => {
                 </div>
               </div>
             ))}
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
